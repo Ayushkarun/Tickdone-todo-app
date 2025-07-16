4623b718f0b664b7f6954c338eebfb7fb4f23358
Ayushkarun <ayushkarun2580@gmail.com>
2025-07-16 10:39:53 +0530

    Stop tracking api_service.dart

--- lib/Service/api_service.dart
+++
@@ -1,4 +0,0 @@
-class Apiservice{
-static const register = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAabkwoAsDrqHHjd83GebVChCo4Maqp6aU';
-static const login='https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAabkwoAsDrqHHjd83GebVChCo4Maqp6aU';
-}
