4d857c6ed31ecb1060100f3ed6f70983c16a54ae
Ayushkarun <ayushkarun2580@gmail.com>
2025-08-06 16:34:59 +0530

    Account details page

---
+++ lib/Screens/Home/Aboutus.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class Aboutus extends StatelessWidget {
+  const Aboutus({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+
+          children: [
+            Image.asset(
+              "assets/images/Logo/logofinal.png",
+              width: 0.15.sw,
+              height: 0.10.sh,
+            ),
+            SizedBox(height: 5.h),
+            Center(
+              child: Text(
+                'TICKDONE',
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25.sp,
+                ),
+              ),
+            ),
+            SizedBox(height: 5.h),
+            Center(
+              child: Text(
+                'Tick a Task,Get it Done.',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Poppins',
+                  fontSize: 13.sp,
+                ),
+              ),
+            ),
+            SizedBox(height: 5.h),
+            Center(
+              child: Padding(
+                padding: EdgeInsets.all(10.w),
+                child: Text(
+                  'TickDone is a Flutter-based Todo app developed by Ayush Karun as part of his project, aimed at helping users manage their daily tasks efficiently. This project was also an opportunity to learn and showcase my Flutter development skills.',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Poppins',
+                    fontSize: 15.sp,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+            SizedBox(height: 5.h),
+            Center(
+              child: Text(
+                'Contact: ayushkarun2580@gmail.com',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Poppins',
+                  fontSize: 11.sp,
+                ),
+              ),
+            ),
+            SizedBox(height: 5.h),
+            Center(
+              child: IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: Icon(
+                  Icons.arrow_back_ios_new_rounded,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/Screens/Home/Account.dart
+++ lib/Screens/Home/Account.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tickdone/Screens/Login/login.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'Aboutus.dart';
+// import 'package:url_launcher/url_launcher.dart';
 
 class Account extends StatefulWidget {
   const Account({super.key});
@@ -11,138 +13,166 @@ class Account extends StatefulWidget {
 }
 
 class _AccountState extends State<Account> {
+ Future<void> helpalert(BuildContext context) async {
+  return showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        backgroundColor: Colors.black,
+        shape: RoundedRectangleBorder(
+          
+          borderRadius: BorderRadius.circular(15.r),
+        ),
+        title: Text(
+          'Send Feedback?',
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: 'Poppins',
+            fontSize: 20.sp,
+          ),
+        ),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Please Mail us on\n',
+              style: TextStyle(color: Colors.white70, fontSize: 14.sp),
+            ),
+            Text(
+              'tickdoneapp@gmail.com',
+              style: TextStyle(color: Colors.lightBlueAccent, fontSize: 14.sp),
+            ),
+          ],
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text('Ok', style: TextStyle(color:Colors.white,fontFamily: 'Poppins')),
+          ),
+         
+        ],
+      );
+    },
+  );
+}
+
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(
-        top: 50.h,
-        bottom: 18.h,
-        left: 10.w,
-        right: 10.w,
-      ),
-      color: Colors.black,
-      height: double.infinity,
-      width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          CircleAvatar(
-            radius: 30.r,
-            backgroundColor: Colors.white,
-            child: Icon(Icons.person, color: Colors.black, size: 60.sp),
-          ),
-
-          SizedBox(height: 10.h),
-          Text(
-            'Account',
-            style: TextStyle(
-              color: Colors.white,
-              fontFamily: 'Poppins',
-              fontWeight: FontWeight.bold,
-              fontSize: 28.sp,
-            ),
-          ),
-          SizedBox(height: 40.h),
-          Center(
+    return SafeArea(
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          return Container(
+            padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 18.h),
+            color: Colors.black,
+            height: constraints.maxHeight, // Use available height
+            width: double.infinity,
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment:
+                  MainAxisAlignment.spaceBetween, // Distribute space
               children: [
-                TextButton(
-                  onPressed: () {},
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.person, color: Colors.white, size: 30.sp),
-                      SizedBox(width: 5.w),
-                      Text(
+                Column(
+                  children: [
+                    SizedBox(height: 20.h),
+                    CircleAvatar(
+                      radius: 35.r,
+                      backgroundColor: Colors.white,
+                      child: Icon(
+                        Icons.person,
+                        color: Colors.black,
+                        size: 40.sp,
+                      ),
+                    ),
+                    SizedBox(height: 10.h),
+                    Text(
+                      'Account',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24.sp,
+                      ),
+                    ),
+                    SizedBox(height: 30.h),
+                    TextButton.icon(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.person,
+                        color: Colors.white,
+                        size: 24.sp,
+                      ),
+                      label: Text(
                         'Change Name',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'Poppins',
-                          fontSize: 16.sp,
-                        ),
+                        style: TextStyle(color: Colors.white, fontSize: 16.sp),
                       ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 8.h),
-                TextButton(
-                  onPressed: () {},
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.key, color: Colors.white, size: 30.sp),
-                      SizedBox(width: 5.w),
-                      Text(
+                    ),
+                    TextButton.icon(
+                      onPressed: () {},
+                      icon: Icon(Icons.key, color: Colors.white, size: 24.sp),
+                      label: Text(
                         'Change Password',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'Poppins',
-                          fontSize: 16.sp,
-                        ),
+                        style: TextStyle(color: Colors.white, fontSize: 16.sp),
                       ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 8.h),
-                TextButton(
-                  onPressed: () {},
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.info, color: Colors.white, size: 30.sp),
-                      SizedBox(width: 5.w),
-                      Text(
+                    ),
+                    TextButton.icon(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => Aboutus()),
+                        );
+                      },
+                      icon: Icon(Icons.info, color: Colors.white, size: 24.sp),
+                      label: Text(
                         'About Us',
+                        style: TextStyle(color: Colors.white, fontSize: 16.sp),
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        helpalert(context);
+                      },
+                      child: Text(
+                        'Help & Feedback',
                         style: TextStyle(
                           color: Colors.white,
                           fontFamily: 'Poppins',
                           fontSize: 16.sp,
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                    SizedBox(height: 10.h),
+                    TextButton.icon(
+                      onPressed: () async {
+                        final prefs = await SharedPreferences.getInstance();
+                        await prefs.clear();
+
+                        Navigator.pushAndRemoveUntil(
+                          context,
+                          MaterialPageRoute(builder: (context) => Loginpage()),
+                          (route) => false,
+                        );
+                      },
+                      icon: Icon(
+                        Icons.logout_sharp,
+                        color: Colors.red,
+                        size: 24.sp,
+                      ),
+                      label: Text(
+                        'Log out',
+                        style: TextStyle(color: Colors.red, fontSize: 16.sp),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-          ),
-          SizedBox(height: 100.h),
-          Center(
-            child: TextButton(
-              onPressed: () {},
-              child: Text(
-                'Help & Feedback',
-                style: TextStyle(color: Colors.white, fontFamily: 'Poppins',fontSize: 16.sp,),
-              ),
-            ),
-          ),
-         SizedBox(height: 80.h),
-          Center(
-            child: TextButton(
-              onPressed: () async {
-                final prefs = await SharedPreferences.getInstance();
-                await prefs.clear();
-
-                Navigator.pushAndRemoveUntil(
-                  context,
-                  MaterialPageRoute(builder: (context) => Loginpage()),
-                  (route) => false,
-                );
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(Icons.logout_sharp, color: Colors.red,size: 25.sp),
-                  SizedBox(width: 2.w),
-                  Text(
-                    'Log out',
-                    style: TextStyle(color: Colors.red, fontFamily: 'Poppins',fontSize: 16.sp,),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
+          );
+        },
       ),
     );
   }
